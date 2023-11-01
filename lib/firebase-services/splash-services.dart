5e00e7dff88f32403acd382f07ac4d52ef3ee392
Mudakir <mudakkirafridi@gmail.com>
2023-11-01 11:26:54 +0500

    some basic changes

--- lib/firebase-services/splash-services.dart
+++ lib/firebase-services/splash-services.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter_firebase/firestore-crud/post-screen.dart';
-import 'package:flutter_firebase/pages/post-screen.dart';
 import 'package:flutter_firebase/pages/sign-up.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
--- lib/firestore-crud/firestore-post-screen.dart
+++ lib/firestore-crud/firestore-post-screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_firebase/utilities/utils.dart';
 import 'package:flutter_firebase/widgets/roundBtn.dart';
 
